d7b6d0dc1e78341903a222226ecdece1be50bdaf
Diogo Antunes <diogo.santiago.pt@gmail.com>
2024-02-26 14:48:55 +0000

    tests: setup unit testing

---
+++ HDSLedger/Service/src/test/java/pt/ulisboa/tecnico/hdsledger/DummyTest.java
@@ -0,0 +1,11 @@
+package pt.ulisboa.tecnico.hdsledger;
+
+import org.junit.jupiter.api.Test;
+
+public class DummyTest {
+	
+	@Test
+	public void easyTest() {
+		System.out.println("easy test print");
+	}
+}
