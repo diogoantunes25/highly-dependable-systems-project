f59e190a4bcf336f1e4e0f298dc6eee735ee45a4
Diogo Melita <dgmelita9@gmail.com>
2024-03-27 19:31:28 +0000

    add benchmarking of load test with multiple clients

--- HDSLedger/Client/multiple-clients.sh
+++ HDSLedger/Client/multiple-clients.sh
@@ -7,21 +7,22 @@
 # execute <txs> transactions per client
 
 usage() {
-    echo "Usage: $0 <replicas> <clients> <txs>"
-    echo "  <clients> - number of clients to run"
-    echo "  <txs> - number of transactions to run per client"
+    echo "Usage: $0 <first-client-id> <last-client-id> <txs>"
+    echo "  <first-client-id> - id of the first client to run"
+    echo "  <last-client-id> - id of the last client to run"
+    echo "  <txs> - number of transactions to run per clients"
     exit 1
 }
 
 # Check the number of arguments
 
-if [ "$#" -ne 2 ]; then
+if [ "$#" -ne 3 ]; then
     usage
 fi
 
-replicas = $1
-clients = $2
-txs = $3
+first=$1
+last=$2
+txs=$3
 
 # Create directory if it doesn't exist
 cd ..
@@ -29,9 +30,10 @@ mkdir -p outputs
 cd Client
 
 # Loop from the number of replicas to the number of clients specified
-for ((i = $replicas; i < $clients; i++)); do
+for ((i=$first; i<=$last; i++)); do
     # Execute mvn command for each value of i, filter and save only the required lines
-    mvn exec:java -DmainClass=pt.ulisboa.tecnico.hdsledger.client.loader.LoaderClient -Dexec.args="${i} $2" | grep -E 'Finished load|Mean Latency|Throughput' | tail -n 3 > "outputs/output${i}.txt" &
+    rm ../outputs/output${i}.txt
+    mvn exec:java -DmainClass=pt.ulisboa.tecnico.hdsledger.client.loader.LoaderClient -Dexec.args="${i} $txs" | grep -E 'Finished load|Mean Latency|Throughput' | tail -n 3 > "../outputs/output${i}.txt" &
 done
 
 # Wait for all background processes to finish
