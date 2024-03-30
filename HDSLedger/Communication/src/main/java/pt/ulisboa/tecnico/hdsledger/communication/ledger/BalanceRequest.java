a364447826e8d5113726cf3fd8ab506cd41215cc
Carolina Coelho <carolina.coelho@tecnico.ulisboa.pt>
2024-03-30 10:21:30 +0000

    test: added checkBalance test

--- HDSLedger/Communication/src/main/java/pt/ulisboa/tecnico/hdsledger/communication/ledger/BalanceRequest.java
+++ HDSLedger/Communication/src/main/java/pt/ulisboa/tecnico/hdsledger/communication/ledger/BalanceRequest.java
@@ -3,16 +3,8 @@ package pt.ulisboa.tecnico.hdsledger.communication.ledger;
 public class BalanceRequest {
     private int source;
 
-    // sequence number
-    private int seq;
-
-    public BalanceRequest(int seq, int source) {
+    public BalanceRequest(int source) {
         this.source = source;
-        this.seq = seq;
-    }
-
-    public int getSeq() {
-        return this.seq;
     }
 
     public int getSource() {
